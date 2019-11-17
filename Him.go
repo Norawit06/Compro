b9b7caef61b95d58ff483abd1c8f1677258d7d28
Norawit06 <Norawit06@.github.com>
2019-11-17 23:28:34 +0700

    Update Him.go

--- Him.go
+++ Him.go
@@ -8,7 +8,6 @@ func main() {
 	printFullName("Babel", "Coder")
 }
 
-// โปรดสังเกต ชื่อตัวแปรจะมาก่อนชนิดข้อมูล
 func printFullName(firstName string, lastName string) {
 	fmt.Println(firstName + " " + lastName)
 }
