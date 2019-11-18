792c1de0c43d2cb49de117677b68eca82f8370ba
Norawit06 <Norawit06@.github.com>
2019-11-18 21:17:41 +0700

    Create w2_vartest.go

---
+++ w2_vartest.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string
+	var num int
+	var price float32
+	var status bool
+
+	fmt.Println(name)
+	fmt.Println(num)
+	fmt.Println(price)
+	fmt.Println(status)
+}
