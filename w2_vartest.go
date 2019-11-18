12a1b9a9ff69ee9eed26058cc3bd4fa018581896
Norawit06 <Norawit06@.github.com>
2019-11-18 21:29:46 +0700

    Update w2_vartest.go
    ลองกำหนดค่าเข้าไปจากตอนแรกที่ไม่กำหนดค่า

--- w2_vartest.go
+++ w2_vartest.go
@@ -5,9 +5,13 @@ import "fmt"
 func main() {
 
 	var name string
+	name = "earth"
 	var num int
+	num = 25
 	var price float32
+	price = 10.99
 	var status bool
+	status = true
 
 	fmt.Println(name)
 	fmt.Println(num)
