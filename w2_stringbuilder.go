0d5c36c1ce0fc1143a5d1e76636d0f6593656c50
Norawit06 <Norawit06@.github.com>
2019-11-20 21:50:06 +0700

    Update w2_stringbuilder.go

--- w2_stringbuilder.go
+++ w2_stringbuilder.go
@@ -10,7 +10,7 @@ func main() {
 
 	startA := time.Now()
 	var a string
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 20000; i++ {
 		a += "a"
 
 	}
@@ -18,7 +18,7 @@ func main() {
 
 	startB := time.Now()
 	var b strings.Builder
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 20000; i++ {
 		b.WriteString("b")
 
 	}
