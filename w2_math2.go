fc42c91a37c7f54d4b77a58aca0eefda67739436
Norawit06 <Norawit06@.github.com>
2019-11-20 21:07:58 +0700

    Create w2_math2.go

---
+++ w2_math2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	Fnumber := 82
+	Fsecond := 75
+	fmt.Println(Fnumber + Fsecond)
+	fmt.Println(Fnumber - Fsecond)
+	fmt.Println(Fnumber / Fsecond)
+
+}
