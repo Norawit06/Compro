10a1bf0a3e12cb371d429a79c72693b65441824b
Norawit06 <Norawit06@.github.com>
2019-11-20 22:00:29 +0700

    Update w2_string2.go

--- w2_string2.go
+++ w2_string2.go
@@ -9,4 +9,8 @@ func main() {
 	fmt.Println(strings.Contains("Pretty Earth", "earth"))
 	fmt.Println(strings.Contains("Pretty Earth", "Earth"))
 
+	fmt.Println(strings.Count("Nice Earth", "e"))
+	fmt.Println(strings.Count("Nice Earth", "h"))
+	fmt.Println(strings.Count("Nice Earth", ""))
+
 }
