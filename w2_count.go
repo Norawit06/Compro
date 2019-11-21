8adbc3a98939b3043bca320a5c5a9547d07569ae
Norawit06 <Norawit06@.github.com>
2019-11-21 20:00:55 +0700

    Update w2_count.go

--- w2_count.go
+++ w2_count.go
@@ -9,4 +9,7 @@ func main() {
 	fmt.Println(strings.Count("apple", "ap"))
 	fmt.Println(strings.Count("LOKLKLMLML", "LM"))
 
+	s := "Hi there!"
+	fmt.Println("The length of s is", len(s))
+	fmt.Println("The first symbol of s is", string(s[0]))
 }
