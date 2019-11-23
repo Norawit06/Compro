53c2eb65195e829ec364d87091b3711e6ede2b87
Norawit06 <Norawit06@.github.com>
2019-11-23 19:24:54 +0700

    Update w2_pointer.go

--- w2_pointer.go
+++ w2_pointer.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var i string
+	var i, j string
 	var p *string
-
 	i = "Earth"
+	j = "Peach"
 
 	fmt.Printf("i = %c\n", i)
 	fmt.Printf("address of i %v\n", &i)
+	fmt.Printf("address of j %v\n", &j)
 	p = &i
+	fmt.Printf("p = %v\n", p)
+	fmt.Printf("*p = %v\n", *p)
+	p = &j
 	fmt.Printf("p = %v\n", p)
 	fmt.Printf("*p = %v\n", *p)
 
