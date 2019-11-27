b64ebbc855813420f517e5c343a510432e4bd0dd
Norawit06 <Norawit06@.github.com>
2019-11-27 22:57:23 +0700

    Update w3_2.go

--- w3/w3_2.go
+++ w3/w3_2.go
@@ -3,11 +3,16 @@ package main
 import "fmt"
 
 func fib(n int) int {
-	if n==0 {return 0}
-	if n==1 {return 1}
-	return fib(n-1)+fib(n-2)
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return fib(n-1) + fib(n-2)
 }
 
-func main(){
-      
+func main() {
+	fmt.Printf("fib(10)=%d\n", fib(10))
+
 }
