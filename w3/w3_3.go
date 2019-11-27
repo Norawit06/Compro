50baa61d936fa1ba7a4c3e9e5b3f8abf902b5c94
Norawit06 <Norawit06@.github.com>
2019-11-27 23:13:55 +0700

    Update w3_3.go

--- w3/w3_3.go
+++ w3/w3_3.go
@@ -5,7 +5,11 @@ import "fmt"
 func main() {
 	fmt.Print("input : ")
 	var text string
+	var age int
 	fmt.Scan(&text)
 	fmt.Println(`read "`, text, `"from input`)
+	fmt.Println("input your age :")
+	fmt.Scan(&age)
+	fmt.Printf("your age :%d ", age)
 
 }
