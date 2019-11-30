dfd3fe7cf5619f28485e728602d8fe4823cdd4f8
Norawit06 <Norawit06@.github.com>
2019-12-01 05:09:03 +0700

    Update w3_6_test.go

--- w3/Test1/w3_6_test.go
+++ w3/Test1/w3_6_test.go
@@ -14,7 +14,7 @@ func TestPlus(t *testing.T) {
 
 func TestPrism(t *testing.T) {
 	a := prism(4, 2, 2)
-	if a != 16 {
+	if a != 20 {
 		t.Errorf("Expected result of 3, but it was  %v instead ", a)
 	}
 }
