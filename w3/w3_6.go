c34d61700464f0ef3614f8a19032e990f8552d2b
Norawit06 <Norawit06@.github.com>
2019-12-01 04:33:30 +0700

    Update w3_6.go

--- w3/w3_6.go
+++ w3/w3_6.go
@@ -1,6 +1,6 @@
-package w3
+package main
 
-import "fmt"
+import ("fmt")
 
 func plus(a int, b int) int {
 	return a + b
@@ -21,5 +21,10 @@ func main() {
 	a, b := 1, 2
 	res := plus(a, b)
 	fmt.Println("1 + 2 =", res)
+
+	res = prism(4, 2, 2)
+	fmt.Println("Rectangular prism=", res)
+
+	fmt.Println("------------------------------")
 
 }
