cc79ecfb62fe18abe6d02bd6f423df5f1a4a7b5d
Norawit06 <Norawit06@.github.com>
2019-12-01 05:05:46 +0700

    Update w3_6_test.go

--- w3/Test1/w3_6_test.go
+++ w3/Test1/w3_6_test.go
@@ -1,11 +1,13 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPlus(t *testing.T) {
 	a := plus(1, 2)
 
 	if a != 3 {
-		t.Errorf("Expected result of 3, but it was %v instead ")
+		t.Errorf("Expected result of 3, but it was  %v instead ", a)
 	}
 }
