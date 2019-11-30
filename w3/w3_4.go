30c548acc1b6597668d3f7b6d9c841197ddcf9cc
Norawit06 <Norawit06@.github.com>
2019-12-01 02:21:57 +0700

    Update w3_4.go

--- w3/w3_4.go
+++ w3/w3_4.go
@@ -7,4 +7,8 @@ func main(){
 	var name string
 	var age int
 	n, err :=fmt.Sscanf("Earth is 25 years old", "%s is %d years old", &name, &age)
-	
+	if err ! := {
+		panic(err)
+	}
+	fmt.Printf("%d: %s, %d\n", n,name,age)
+}
