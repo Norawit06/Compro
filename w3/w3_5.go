135c01892761bae0fae806afb280f922ccbe4f49
Norawit06 <Norawit06@.github.com>
2019-12-01 02:31:21 +0700

    Update w3_5.go

--- w3/w3_5.go
+++ w3/w3_5.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	printFirst()
 	printFinish()
 	printSecond()
 
+}
+
+func printFirst() {
+	fmt.Println("First")
+}
+
+func printSecond() {
+	fmt.Println("Second")
+}
+
+func printFinish() {
+	fmt.Println("Close")
 }
