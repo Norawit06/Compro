6d86404a0eec88db33ce233356b3a50d4e7d376b
Norawit06 <Norawit06@.github.com>
2019-12-01 04:28:00 +0700

    Update w3_6.go

--- w3/w3_6.go
+++ w3/w3_6.go
@@ -1,5 +1,15 @@
 package w3
 
+import "fmt"
+
 func plus(a int, b int) int {
 	return a + b
+}
+
+func prism(length, width, height int) int {
+	return length * width * height
+}
+
+func init() {
+	fmt.Println("init ...")
 }
