7ee8cfc866cc5839c46dc742593bbbef5b24ed08
Norawit06 <Norawit06@.github.com>
2019-12-01 02:22:23 +0700

    Update w3_4.go

--- w3/w3_4.go
+++ w3/w3_4.go
@@ -1,14 +1,13 @@
 package main
 
-
 import "fmt"
 
-func main(){
+func main() {
 	var name string
 	var age int
-	n, err :=fmt.Sscanf("Earth is 25 years old", "%s is %d years old", &name, &age)
-	if err ! := {
+	n, err := fmt.Sscanf("Earth is 25 years old", "%s is %d years old", &name, &age)
+	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%d: %s, %d\n", n,name,age)
+	fmt.Printf("%d: %s, %d\n", n, name, age)
 }
