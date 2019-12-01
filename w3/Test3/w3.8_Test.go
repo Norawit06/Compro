c682411452fa16efeeadd60391fa76086073d578
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-01 19:47:08 +0700

    Update w3.8_Test.go

--- w3/Test3/w3.8_Test.go
+++ w3/Test3/w3.8_Test.go
@@ -3,9 +3,15 @@ package main
 import "testing"
 
 func TestFib(t *testing.T) {
-	a := fib(77)
-	if a != 55 {
-		t.Errorf("Expect fib is 55 ,but it was %v instead", a)
-	}
+     type args struct{
+		 n int
+	 }
+
+	 tests := []struct{
+		 name string
+		 args args
+	 }
+	 
+	
 
 }
