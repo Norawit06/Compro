efb99b72f5f431d1a4405f11af91deba2d3033b4
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-01 19:47:06 +0700

    Update w3_8.go

--- w3/Test3/w3_8.go
+++ w3/Test3/w3_8.go
@@ -2,18 +2,18 @@ package main
 
 import "fmt"
 
-func fib(n int) int {
+func Fib(n int) int {
 	if n == 0 {
 		return 0
 	}
 	if n == 1 {
 		return 1
 	}
-	return fib(n-1) + fib(n-2)
+	return Fib(n-1) + Fib(n-2)
 
 }
 func main() {
-	fmt.Printf("fib(10)=%d\n", fib(10))
-	fmt.Printf("fib(11)=%d\n", fib(11))
-	fmt.Printf("fib(12)=%d\n", fib(12))
+	fmt.Printf("fib(10)=%d\n", Fib(10))
+	fmt.Printf("fib(11)=%d\n", Fib(11))
+	fmt.Printf("fib(12)=%d\n", Fib(12))
 }
