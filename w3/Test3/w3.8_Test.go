1cdc0b26d844a397bfd57d78f108ca9d27bef5b4
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-01 19:50:49 +0700

    Update w3.8_Test.go

--- w3/Test3/w3.8_Test.go
+++ w3/Test3/w3.8_Test.go
@@ -7,11 +7,18 @@ func TestFib(t *testing.T) {
 		 n int
 	 }
 
-	 tests := []struct{
+	 tests := []struct {
 		 name string
 		 args args
+		 want int
 	 }
-	 
+	for _, tt := range tests {
+		t.run(tt.name, func(t *testing.T)){
+			if got := fib(tt.args.n); got != tt.want{
+				t.Errorf("fib()= %v, want %v", got, tt.want)
+			}
+		}
+	}
 	
 
 }
