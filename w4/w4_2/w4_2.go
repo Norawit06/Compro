e5ddd49b889deeca65ef40d588b3d43110194664
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-05 21:38:06 +0700

    Update w4_2.go

--- w4/w4_2/w4_2.go
+++ w4/w4_2/w4_2.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func hello(user string){ string{
-	if(len( user ) == 0){
+func Hello(user string){ string{
+	if( len( user ) == 0){
 		return "hello man!"
 	} else {
-		return fmt.Sprintf("hello %v!", user)
+		return fmt.Sprintf("hello %v!", user )
 	}
 
 	}
