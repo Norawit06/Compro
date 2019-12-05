071255113adb28eb0d070aca51bab1b7c43b53c1
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-05 21:48:12 +0700

    Update w4_2.go

--- w4/w4_2/w4_2.go
+++ w4/w4_2/w4_2.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func Hello(user string){ string{
-	if( len( user ) == 0){
+	if( len( user ) == 0) {
 		return "hello man!"
 	} else {
 		return fmt.Sprintf("hello %v!", user )
 	}
 
-	}
+	
 
 
 }
