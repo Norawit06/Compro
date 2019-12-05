bffa252ac206761c2e969c97f82b48f1fe94f49b
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-05 21:45:16 +0700

    Update w4_2_test.go

--- w4/w4_2/w4_2_test.go
+++ w4/w4_2/w4_2_test.go
@@ -6,6 +6,9 @@ func TestHello(t *testing.T) {
 
 	result := Hello("Per") // Should return "hello Per!"
 
-	if result != "heloo Per!"
+	if result != "heloo Per!"{
+		t.Errorf("hello(\"Per\") failed, expected %v, got %v, ""hello man!", result) 
+	} else {
+		t.Logf("hello(\"hello Per!\") success,
+	}
 }
-
