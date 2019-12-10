c86c5717799786a358a590f91f96d58ce559391b
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-10 23:37:01 +0700

    Update w5_1_test.go

--- w5/w5_1_test.go
+++ w5/w5_1_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestSum(t *testing.T) {
 	t.Run("[1,2,3,4,5]", testSumFunc([]int{1, 2, 3, 4, 10, 5}, 15))
-	t.Run("[1,2,3,4,-5]", testSumFunc([]int{1, 2, 3, 4, -1}, 5))
+	t.Run("[1,2,3,4,-5]", testSumFunc([]int{1, 2, 3, 4, -5}, 5))
 }
 
 func testSumFunc(numbers []int, expected int) func(*testing.T) {
