f265a9c1abd299be2c0b414e40daff6ed54b8e13
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-10 23:25:35 +0700

    Update w5_1_test.go

--- w5/w5_1_test.go
+++ w5/w5_1_test.go
@@ -1,19 +1,20 @@
 package w5
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	number := []int{1, 2, 3, 4, 5, 5}
-	expected := 15
-	actual := Sum(number)
-
-	if actual != expected {
-		t.Errorf("expected the sum of %v to be %d but instead got %d!", number, expected, actual)
-	}
+	t.Run("[1,2,3,4,5]", testSumFunc([]int{1, 2, 3, 4, 5}, 15))
+	t.Run("[1,2,3,4,-5]", testSumFunc([]int{1, 2, 3, 4, -5}, 5))
 }
 
 func testSumFunc(numbers []int, expected int) func(*testing.T) {
 	return func(t *testing.T) {
-	  actual := Sum(numbers)
-	  if actual != expected {
-		t.Error(fmt.Sprintf("Expected the sum of %v to be %d but instead got %d!", numbers, expected, actual))
+		actual := Sum(numbers)
+		if actual != expected {
+			t.Error(fmt.Sprintf("Expected the sum of %v to be %d but instead got %d!", numbers, expected, actual))
+		}
+	}
+}
