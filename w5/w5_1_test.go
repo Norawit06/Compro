9c830aff532bd828c273eb5e59260ceae909ac62
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-10 23:13:18 +0700

    Update w5_1_test.go

--- w5/w5_1_test.go
+++ w5/w5_1_test.go
@@ -6,4 +6,8 @@ func TestSum(t *testing.T) {
 	number := []int{1, 2, 3, 4, 5}
 	expected := 15
 	actual := Sum(number)
+
+	if actual != expected{
+		t.Errorf("expected the sum of %v to be %d but instead got %d!", number,expected,actual
+	}
 }
