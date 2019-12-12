68264aa22e1cd3e2046ece163c778d4b0ab02216
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-12 21:15:12 +0700

    Update w5_2_test.go

--- w5/w5_2/w5_2_test.go
+++ w5/w5_2/w5_2_test.go
@@ -2,6 +2,16 @@ package main
 
 import "testing"
 
+type AddResult struct {
+	x        int
+	y        int
+	expected int
+}
+
+var AddResult = []AddResult{
+	{1, 1, 2},
+}
+
 func TestAdd(t *testing.T) {
 
 }
