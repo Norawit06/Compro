b06da1edbefdaa9713efb367fe56b45f08ed5c5d
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-15 00:57:52 +0700

    Update w5_3_test.go

--- w5/w5_3/w5_3_test.go
+++ w5/w5_3/w5_3_test.go
@@ -2,10 +2,11 @@ package w5_3
 
 import "testing"
 
-func TestaddMult(t *testing.T) {
-	a := addMult(7 * 5)
-
-	if a != 17 {
+func TestaddMult(t *testing.T)
+{
+	a := addMult(7, 5)
+	 
+	if a != 14, 35{
 		t.Errorf("want 17, got%v", a)
 	}
 }
