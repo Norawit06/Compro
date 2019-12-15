b8b7ac79edd559dc05f57717fceade248c4f07d9
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-15 22:20:20 +0700

    Update w5_5.go

--- w5/w5_5/w5_5.go
+++ w5/w5_5/w5_5.go
@@ -1,12 +1,18 @@
 package w5_5
 
 func Find(a []int, x int) int {
-    switch len(a) {
-    case 0:
-        return 0
-    case 1:
-        if x <= a[0] {
-            return 0
-        }
-        return 1
-    }
+	switch len(a) {
+	case 0:
+		return 0
+	case 1:
+		if x <= a[0] {
+			return 0
+		}
+		return 1
+	}
+	mid := len(a) / 2
+	if x <= a[mid-1] {
+		return Find(a[:mid], x)
+	}
+	return mid + Find(a[mid:], x)
+}
