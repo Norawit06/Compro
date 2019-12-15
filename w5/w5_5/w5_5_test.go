90da179367b45c5f741585f386f3c75a33b243e3
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-15 22:23:04 +0700

    Update w5_5_test.go

--- w5/w5_5/w5_5_test.go
+++ w5/w5_5/w5_5_test.go
@@ -2,6 +2,19 @@ package w5_5
 
 import "testing"
 
+var tests = []struct {
+	a   []int
+	x   int
+	exp int
+}{
+	{[]int{}, 1, 0},
+	{[]int{1, 2, 3, 3}, 0, 0},
+	{[]int{1, 2, 3, 3}, 1, 0},
+	{[]int{1, 2, 3, 3}, 2, 1},
+	{[]int{1, 2, 3, 3}, 3, 3}, // incorrect test case
+	{[]int{1, 2, 3, 3}, 4, 4},
+}
+
 func TestFind(t *testing.T) {
 	for _, e := range tests {
 		res := Find(e.a, e.x)
