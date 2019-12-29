85d368034f37aed0e198a1f73139d2090aaeba14
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-29 16:30:55 +0700

    Update w7_5.go

--- w7/w7_5/w7_5.go
+++ w7/w7_5/w7_5.go
@@ -1,22 +1,25 @@
-package w7_5
+package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Employee struct {
 	firstName, lastName string
-	age, salary         interface
+	age, salary         int
 }
 
 func main() {
 
 	emp1 := Employee{
 		firstName: "Sam",
-		age:        25,
-		salary      1500,
-		lastName    "Anderson",
+		age:       25,
+		salary:    500,
+		lastName:  "Anderson",
 	}
-	emp2 := Employee{"Jim", "paul", 29, 9000}
 
-	fmt.Println("Employee 1 ", emp1)
-	fmt.Println("Employee 2 ", emp2)
+	emp2 := Employee{"Thomas", "Paul", 29, 800}
+
+	fmt.Println("Employee 1", emp1)
+	fmt.Println("Employee 2", emp2)
 }
