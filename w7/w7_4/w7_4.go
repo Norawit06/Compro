7231c7421cfa686b3bb50884d6bb901576ba7ff1
Norawit06 <norawit.pa@rmuti.ac.th>
2019-12-29 16:13:12 +0700

    Update w7_4.go

--- w7/w7_4/w7_4.go
+++ w7/w7_4/w7_4.go
@@ -13,3 +13,16 @@ func prism(length, width, height int) int {
 func init() {
 	fmt.Println("Init ...")
 }
+
+func main() {
+	fmt.Println("WEEK 7 START")
+	fmt.Println("-------------------------------")
+
+	a, b := 1, 2
+	res := plus(a, b)
+	fmt.Println("1 + 2 =", res)
+
+	res = prism(4, 2, 2)
+	fmt.Println("Rectangular prism=", res)
+	fmt.Println("-------------------------------")
+}
